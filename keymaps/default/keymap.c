7a373c8fe458eba284ff8af150dbf4c8338bf90d
Samuel Born <samuelborn@outlook.de>
2025-07-05 12:41:42 +0200

    big refactor: unify layers without homerow mods

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -2,10 +2,8 @@
 
 enum layers {
     _BASE,
-    _SYMBOLS_RIGHT,
-    _SYMBOLS_LEFT,
+    _SYMBOLS,
     _NAV,
-    _NUMPAD,
     _FUNCTION,
 };
 
@@ -18,49 +16,42 @@ enum custom_keycodes {
     SEL_LINE,
 };
 
+
+
+
+
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-                 KC_Q,         KC_L,         KC_D,         KC_C,         KC_V,           KC_J,    KC_F,         KC_O,         KC_U,         KC_DOT,
-                 LGUI_T(KC_N), LALT_T(KC_R), LCTL_T(KC_T), LSFT_T(KC_S), KC_G,           KC_Y,    RSFT_T(KC_H), LCTL_T(KC_A), LALT_T(KC_E), LGUI_T(KC_I),
-        KC_RALT, KC_Z,         KC_X,         KC_M,         KC_W,         KC_B,           KC_K,    KC_P,         KC_QUOTE,     KC_SEMICOLON, KC_COMMA,     KC_LGUI,
-        LT(_FUNCTION, KC_TAB), LT(_SYMBOLS_RIGHT, KC_SPACE), LT(_NAV, KC_DELETE),        LT(_NUMPAD, KC_ENTER), LT(_SYMBOLS_LEFT, KC_BACKSPACE), LT(_FUNCTION, KC_ESCAPE)
+                       KC_Q, KC_L,         KC_D,         KC_C,         KC_V,                KC_J,    KC_F,         KC_O,         KC_U,         KC_DOT,
+               LGUI_T(KC_N), LALT_T(KC_R), LCTL_T(KC_T), LSFT_T(KC_S), KC_G,                KC_Y,    RSFT_T(KC_H), LCTL_T(KC_A), LALT_T(KC_E), LGUI_T(KC_I),
+              KC_RALT, KC_Z, KC_X,         KC_M,         KC_W,         KC_B,                KC_K,    KC_P,         KC_QUOT,      KC_SCLN,      KC_COMM, KC_LGUI,
+              LT(_FUNCTION, KC_TAB), LT(_SYMBOLS, KC_SPC), LT(_NAV, KC_ENT),                LT(_NAV, KC_ESC), LT(_SYMBOLS, KC_BSPC), LT(_FUNCTION, KC_DEL)
     ),
 
-    [_SYMBOLS_RIGHT] = LAYOUT(
-                 _______, _______, _______, _______, _______,               KC_PIPE, KC_LBRC, KC_RBRC, KC_GRAVE, _______,
-                 KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,               KC_HASH, KC_LPRN, KC_RPRN, KC_EXCLAIM, KC_QUESTION,
-        _______, _______, _______, _______, _______, _______,               KC_AT, KC_LCBR, KC_RCBR, KC_DOLLAR, RALT(KC_E), _______,
-                                   _______, _______, _______,               _______, _______, _______
-    ),
-
-    [_SYMBOLS_LEFT] = LAYOUT(
-                 KC_BACKSLASH, KC_LT, KC_GT, KC_MINUS, KC_PERCENT,          _______, _______, _______, _______, _______,
-                 KC_ASTERISK, KC_PLUS, KC_SLASH, KC_EQUAL, KC_AMPERSAND,    _______, KC_RSFT, KC_LCTL, KC_LALT, KC_LGUI,
-        _______, KC_CIRC, KC_TILDE, CURR_DIR, UP_DIR, HOME_DIR,             _______, _______, _______, _______, _______, _______,
-                                     _______, _______, _______,             _______, _______, _______
+    [_SYMBOLS] = LAYOUT(
+                               KC_BSLS, KC_LT,   KC_GT,    KC_MINS, KC_PERC,                KC_PIPE, KC_LBRC, KC_RBRC, KC_GRV,  _______,
+                               KC_ASTR, KC_PLUS, KC_SLSH,  KC_EQL,  KC_AMPR,                KC_HASH, KC_LPRN, KC_RPRN, KC_EXLM, KC_QUES,
+                      KC_LGUI, KC_CIRC, KC_TILD, CURR_DIR, UP_DIR, HOME_DIR,                KC_AT,   KC_LCBR, KC_RCBR, KC_DLR,  RALT(KC_E), KC_LGUI,
+                                                 KC_LALT,  KC_LCTL, KC_LSFT,                KC_RSFT, KC_LCTL, KC_LALT
     ),
 
     [_NAV] = LAYOUT(
-                 _______, _______, _______, _______, _______,               KC_PGUP,  KC_HOME,  _______, KC_END,   _______,
-                 KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,               KC_PGDN,  KC_LEFT,  KC_UP,   KC_RIGHT, _______,
-        _______, _______, _______, _______, _______, _______,               SEL_LINE, SEL_BACK, KC_DOWN, SEL_FWD,  _______, _______,
-                                   _______, _______, _______,               _______,  _______,  _______
-    ),
-
-    [_NUMPAD] = LAYOUT(
-                 KC_CIRC,     KC_9, KC_8,   KC_7,   _______,                _______, _______, _______, _______, _______,
-                 KC_ASTERISK, KC_3, KC_2,   KC_1,   KC_PLUS,                _______, KC_RSFT, KC_LCTL, KC_LALT, KC_LGUI,
-        _______, KC_SLASH,    KC_6, KC_5,   KC_4,   KC_MINUS,               _______, _______, _______, _______, _______, _______,
-                                   KC_DOT,  KC_0,   KC_COMMA,               _______, _______, _______
+                 KC_CIRC, KC_9,         KC_8,         KC_7,         _______,                KC_PGUP,  KC_HOME,         _______,       KC_END,          _______,
+         LGUI_T(KC_PAST), LALT_T(KC_3), LCTL_T(KC_2), LSFT_T(KC_1), KC_PLUS,                KC_PGDN,  RSFT_T(KC_LEFT), LCTL_T(KC_UP), LALT_T(KC_RGHT), KC_LGUI,
+        _______, KC_SLSH, KC_6,         KC_5,         KC_4,         KC_MINS,                SEL_LINE, SEL_BACK,        KC_DOWN,       SEL_FWD,         _______, _______,
+                                                      KC_DOT, KC_0, KC_COMM,                _______,  _______,  _______
     ),
 
     [_FUNCTION] = LAYOUT(
-                 KC_F10,  KC_F9,   KC_F8,   KC_F7,   _______,               _______, KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP, KC_PRINT_SCREEN, _______,
-                 KC_F11,  KC_F3,   KC_F2,   KC_F1,   _______,               _______, KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP, KC_MUTE, KC_PRINT_SCREEN,
-        _______, KC_F12,  KC_F6,   KC_F5,   KC_F4,   _______,               _______, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, KC_MEDIA_PLAY_PAUSE, _______, _______,
-                                   _______, _______, _______,               _______, _______, _______
+                                       KC_F10, KC_F9, KC_F8, KC_F7, _______,                _______, KC_BRID, KC_BRIU, KC_PSCR, _______,
+                                       KC_F11, KC_F3, KC_F2, KC_F1, _______,                _______, KC_VOLD, KC_VOLU, KC_MUTE, KC_PSCR,
+                              _______, KC_F12, KC_F6, KC_F5, KC_F4, _______,                _______, KC_MPRV, KC_MNXT, KC_MPLY, _______, _______,
+                                                  _______, _______, _______,                _______, _______, _______
     )
 };
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {

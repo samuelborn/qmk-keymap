0bb5b41740ef64b53c00f48cd8302ae61e515edf
Samuel Born <samuelborn@outlook.de>
2025-07-06 20:05:59 +0200

    workaround: add homerow mods on symbol layer

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -7,6 +7,12 @@ enum layers {
     _FUNCTION,
 };
 
+// workaround for mod tap with non basic keycodes
+#define MOD_LPRN RSFT_T(KC_LPRN)
+#define MOD_RPRN LCTL_T(KC_RPRN)
+#define MOD_EXLM LALT_T(KC_EXLM)
+#define MOD_QUES LGUI_T(KC_QUES)
+
 enum custom_keycodes {
     UP_DIR = SAFE_RANGE,
     CURR_DIR,
@@ -18,31 +24,31 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-                       KC_Q, KC_L,         KC_D,         KC_C,         KC_V,                KC_J,    KC_F,         KC_O,         KC_U,         KC_DOT,
-               LGUI_T(KC_N), LALT_T(KC_R), LCTL_T(KC_T), LSFT_T(KC_S), KC_G,                KC_Y,    RSFT_T(KC_H), LCTL_T(KC_A), LALT_T(KC_E), LGUI_T(KC_I),
-              KC_RALT, KC_Z, KC_X,         KC_M,         KC_W,         KC_B,                KC_K,    KC_P,         KC_QUOT,      KC_SCLN,      KC_COMM, KC_LGUI,
-              LT(_FUNCTION, KC_TAB), LT(_SYMBOLS, KC_SPC), LT(_NAV, KC_ENT),                LT(_NAV, KC_ESC), LT(_SYMBOLS, KC_BSPC), LT(_FUNCTION, KC_DEL)
+                       KC_Q, KC_L,         KC_D,         KC_C,         KC_V,        KC_J,    KC_F,         KC_O,         KC_U,         KC_DOT,
+               LGUI_T(KC_N), LALT_T(KC_R), LCTL_T(KC_T), LSFT_T(KC_S), KC_G,        KC_Y,    RSFT_T(KC_H), LCTL_T(KC_A), LALT_T(KC_E), LGUI_T(KC_I),
+              KC_RALT, KC_Z, KC_X,         KC_M,         KC_W,         KC_B,        KC_K,    KC_P,         KC_QUOT,      KC_SCLN,      KC_COMM, KC_LGUI,
+              LT(_FUNCTION, KC_TAB), LT(_SYMBOLS, KC_SPC), LT(_NAV, KC_ENT),        LT(_NAV, KC_ESC), LT(_SYMBOLS, KC_BSPC), LT(_FUNCTION, KC_DEL)
     ),
 
     [_SYMBOLS] = LAYOUT(
-                               KC_BSLS, KC_LT,   KC_GT,    KC_MINS, KC_PERC,                KC_PIPE, KC_LBRC, KC_RBRC, KC_GRV,  _______,
-                               KC_ASTR, KC_PLUS, KC_SLSH,  KC_EQL,  KC_AMPR,                KC_HASH, KC_LPRN, KC_RPRN, KC_EXLM, KC_QUES,
-                      KC_LGUI, KC_CIRC, KC_TILD, CURR_DIR, UP_DIR, HOME_DIR,                KC_AT,   KC_LCBR, KC_RCBR, KC_DLR,  RALT(KC_E), KC_LGUI,
-                                                 KC_LALT,  KC_LCTL, KC_LSFT,                KC_RSFT, KC_LCTL, KC_LALT
+                               KC_BSLS, KC_LT,   KC_GT,    KC_MINS, KC_PERC,        KC_PIPE, KC_LBRC, KC_RBRC, KC_GRV,  _______,
+ LGUI_T(KC_PAST), LALT_T(KC_PPLS), LCTL_T(KC_SLSH), LSFT_T(KC_EQL), KC_AMPR,        KC_HASH, MOD_LPRN, MOD_RPRN, MOD_EXLM, MOD_QUES,
+                      KC_LGUI, KC_CIRC, KC_TILD, CURR_DIR, UP_DIR, HOME_DIR,        KC_AT,   KC_LCBR, KC_RCBR, KC_DLR,  RALT(KC_E), KC_LGUI,
+                                                  _______, _______, _______,        _______, _______, _______
     ),
 
     [_NAV] = LAYOUT(
-                 KC_CIRC, KC_9,         KC_8,         KC_7,         _______,                KC_PGUP,  KC_HOME,         _______,       KC_END,          _______,
-         LGUI_T(KC_PAST), LALT_T(KC_3), LCTL_T(KC_2), LSFT_T(KC_1), KC_PLUS,                KC_PGDN,  RSFT_T(KC_LEFT), LCTL_T(KC_UP), LALT_T(KC_RGHT), KC_LGUI,
-        _______, KC_SLSH, KC_6,         KC_5,         KC_4,         KC_MINS,                SEL_LINE, SEL_BACK,        KC_DOWN,       SEL_FWD,         _______, _______,
-                                                      KC_DOT, KC_0, KC_COMM,                _______,  _______,  _______
+                 KC_CIRC, KC_9,         KC_8,         KC_7,         _______,        KC_PGUP,  KC_HOME,         _______,       KC_END,          _______,
+         LGUI_T(KC_PAST), LALT_T(KC_3), LCTL_T(KC_2), LSFT_T(KC_1), KC_PLUS,        KC_PGDN,  RSFT_T(KC_LEFT), LCTL_T(KC_UP), LALT_T(KC_RGHT), KC_LGUI,
+        _______, KC_SLSH, KC_6,         KC_5,         KC_4,         KC_MINS,        SEL_LINE, SEL_BACK,        KC_DOWN,       SEL_FWD,         _______, _______,
+                                                      KC_DOT, KC_0, KC_COMM,        _______,  _______,  _______
     ),
 
     [_FUNCTION] = LAYOUT(
-                                       KC_F10, KC_F9, KC_F8, KC_F7, _______,                _______, KC_BRID, KC_BRIU, _______, _______,
-       LGUI_T(KC_F11), LALT_T(KC_F3), LCTL_T(KC_F2), LSFT_T(KC_F1), _______,                _______, RSFT_T(KC_VOLD), LCTL_T(KC_VOLU), LALT_T(KC_MUTE), LGUI_T(KC_PSCR),
-                              _______, KC_F12, KC_F6, KC_F5, KC_F4, _______,                _______, KC_MPRV, KC_MNXT, KC_MPLY, _______, _______,
-                                                  _______, _______, _______,                _______, _______, _______
+                                       KC_F10, KC_F9, KC_F8, KC_F7, _______,        _______, KC_BRID, KC_BRIU, _______, _______,
+       LGUI_T(KC_F11), LALT_T(KC_F3), LCTL_T(KC_F2), LSFT_T(KC_F1), _______,        _______, RSFT_T(KC_VOLD), LCTL_T(KC_VOLU), LALT_T(KC_MUTE), LGUI_T(KC_PSCR),
+                              _______, KC_F12, KC_F6, KC_F5, KC_F4, _______,        _______, KC_MPRV, KC_MNXT, KC_MPLY, _______, _______,
+                                                  _______, _______, _______,        _______, _______, _______
     )
 };
 
@@ -69,6 +75,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 tap_code(KC_HOME);
                 tap_code16(LSFT(KC_END));
                 return false;
+            case MOD_LPRN:
+                if (record->tap.count > 0 && record->event.pressed) {
+                    tap_code16(KC_LPRN);
+                    return false;  
+                }
+                break;
+            case MOD_RPRN:
+                if (record->tap.count > 0 && record->event.pressed) {
+                    tap_code16(KC_RPRN);
+                    return false;  
+                }
+                break;
+            case MOD_EXLM:
+                if (record->tap.count > 0 && record->event.pressed) {
+                    tap_code16(KC_EXLM);
+                    return false;  
+                }
+                break;
+            case MOD_QUES:
+                if (record->tap.count > 0 && record->event.pressed) {
+                    tap_code16(KC_QUES);
+                    return false;  
+                }
+                break;
         }
     }
     return true;

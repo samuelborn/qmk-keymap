c5dcc9a7dff74c23b5c0393e8143b15e069a361a
Samuel Born <samuelborn@outlook.de>
2025-07-10 11:49:54 +0200

    add comments

--- keymaps/default/config.h
+++ keymaps/default/config.h
@@ -1,8 +1,11 @@
 #pragma once
 
+// automatically detect which side is plugged in
 #define SPLIT_USB_DETECT
+// needed for keyboard to be recognized with fast boot enabled
 #define SPLIT_USB_TIMEOUT 5000
 
+// Better settings for home row mods
 #define TAPPING_TERM 250
 #define PERMISSIVE_HOLD
 #define CHORDAL_HOLD

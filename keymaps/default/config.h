1c1b8c2d1098ab41822bc18d767939a2bf5f7644
Samuel Born <samuelborn@outlook.de>
2025-09-04 21:59:04 +0200

    fix detection on new system

--- keymaps/default/config.h
+++ keymaps/default/config.h
@@ -1,14 +1,15 @@
 #pragma once
 
-// automatically detect which side is plugged in
+#define MASTER_LEFT
 #define SPLIT_USB_DETECT
-// needed for keyboard to be recognized with fast boot enabled
 #define SPLIT_USB_TIMEOUT 5000
+#define SPLIT_USB_TIMEOUT_POLL 20
+#define SPLIT_WATCHDOG_ENABLE
+#define SPLIT_WATCHDOG_TIMEOUT 6000
 
 // Better settings for home row mods
 #define TAPPING_TERM 225
 #define PERMISSIVE_HOLD
 #define CHORDAL_HOLD
-
-// Better Caps Lock handling
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+

b14e5409ecdc6d53b5192b1cfc46c5c3fc143928
Samuel Born <samuelborn@outlook.de>
2025-11-01 17:10:48 +0100

    remove non home row mod layer + add email shortcut

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -5,7 +5,6 @@ enum layers {
     _L1,
     _L2,
     _L3,
-    _L4,
 };
 
 enum custom_keycodes {
@@ -13,13 +12,14 @@ enum custom_keycodes {
     CURR_DIR,
     HOME_DIR,
     SEL_LINE,
+    EMAIL,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_L0] = LAYOUT(
                        KC_B, KC_L,         KC_D,         KC_C,         KC_V,        KC_J,    KC_F,         KC_O,         KC_U,            KC_DOT,
                LGUI_T(KC_N), LALT_T(KC_R), LCTL_T(KC_T), LSFT_T(KC_S), KC_G,        KC_Y,    RSFT_T(KC_H), LCTL_T(KC_A), LALT_T(KC_E),    LGUI_T(KC_I),
-              KC_LGUI, KC_Q, KC_X,         KC_M,         RALT_T(KC_W), KC_Z,        KC_K,    RALT_T(KC_P), KC_QUOT,      KC_SCLN, KC_COMM, TG(_L4),
+              KC_LGUI, KC_Q, KC_X,         KC_M,         RALT_T(KC_W), KC_Z,        KC_K,    RALT_T(KC_P), KC_QUOT,      KC_SCLN, KC_COMM, KC_LGUI,
                           LT(_L1, KC_TAB), LT(_L2, KC_SPC), LT(_L3, KC_ESC),        LT(_L3, KC_ENT), LT(_L2, KC_BSPC), LT(_L1, KC_DEL)
     ),
 
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_L2] = LAYOUT(
  KC_BSLS,         KC_LT,           KC_GT,           KC_MINS,        KC_UNDS,        KC_PIPE, KC_LBRC,         KC_RBRC,         KC_GRV,          KC_PERC,
  LGUI_T(KC_PAST), LALT_T(KC_PPLS), LCTL_T(KC_SLSH), LSFT_T(KC_EQL), KC_AMPR,        KC_HASH, RSFT_T(KC_LPRN), LCTL_T(KC_RPRN), LALT_T(KC_EXLM), LGUI_T(KC_QUES),
-XXXXXXX, KC_CIRC, KC_TILD,         HOME_DIR,        CURR_DIR,        UP_DIR,        KC_AT,   KC_LCBR,         KC_RCBR,         KC_DLR,          RALT(KC_E), XXXXXXX,
+XXXXXXX, KC_CIRC, KC_TILD,         HOME_DIR,        CURR_DIR,        UP_DIR,        KC_AT,   KC_LCBR,         KC_RCBR,         KC_DLR,          RALT(KC_E), EMAIL,
                                                   _______, _______, _______,        _______, _______, _______
     ),
 
@@ -43,14 +43,6 @@ LGUI_T(KC_PSCR), LALT_T(KC_MUTE), LCTL_T(KC_VOLD), RSFT_T(KC_VOLU), XXXXXXX,    
 KC_PWR, KC_SLEP, KC_MPLY,         KC_MPRV,         KC_MNXT,         XXXXXXX,        XXXXXXX, LCTL(KC_LEFT), KC_DOWN,  LCTL(KC_RIGHT), XXXXXXX, XXXXXXX,
                                                   _______, _______, _______,        _______, _______, _______
     ),
-
-    // Disable home row mods: useful for gaming
-    [_L4] = LAYOUT(
-                                               KC_B, KC_L, KC_D, KC_C, KC_V,        KC_J, KC_F, KC_O, KC_U, KC_DOT,
-                                               KC_N, KC_R, KC_T, KC_S, KC_G,        KC_Y, KC_H, KC_A, KC_E, KC_I,
-                                KC_LGUI, KC_Q, KC_X, KC_M, KC_W, KC_Z, KC_K,        KC_P, KC_QUOT, KC_SCLN, KC_COMM, TG(_L4),
-                                                     KC_TAB, KC_SPC, KC_ESC,        KC_ENT, KC_BSPC, KC_DEL
-    )
 };
 
 
@@ -69,6 +61,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             case SEL_LINE:
                 tap_code(KC_HOME);
                 tap_code16(LSFT(KC_END));
+                return false;
+            case EMAIL:
+                send_string("samuelborn@outlook.de");
                 return false;
 
             // Workaround for mod tap with non basic keycodes

bd1ba82d64aad418f8aae79afd0a12ca3da45476
Samuel Born <samuelborn@outlook.de>
2025-11-10 22:13:41 +0100

    simplify

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -7,12 +7,6 @@ enum layers {
     _L3,
 };
 
-enum custom_keycodes {
-    UP_DIR = SAFE_RANGE,
-    HOME_DIR,
-    SEL_LINE,
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
     [_L0] = LAYOUT(
@@ -23,23 +17,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_L1] = LAYOUT(
-XXXXXXX,         XXXXXXX,         KC_BRID,         KC_BRIU,         XXXXXXX,        XXXXXXX, KC_F7,         KC_F8,         KC_F9,         KC_F10,
+        XXXXXXX, XXXXXXX,         KC_BRID,         KC_BRIU,         XXXXXXX,        XXXXXXX, KC_F7,         KC_F8,         KC_F9,         KC_F10,
 LGUI_T(KC_PSCR), LALT_T(KC_MUTE), LCTL_T(KC_VOLD), RSFT_T(KC_VOLU), XXXXXXX,        XXXXXXX, LSFT_T(KC_F1), LCTL_T(KC_F2), LALT_T(KC_F3), LGUI_T(KC_F11),
 KC_PWR, KC_SLEP, KC_MPLY,         KC_MPRV,         KC_MNXT,         XXXXXXX,        XXXXXXX, KC_F4,         KC_F5,         KC_F6,         KC_F12, XXXXXXX,
                                                   _______, _______, _______,        _______, _______, _______
     ),
 
     [_L2] = LAYOUT(
- KC_PERC,         KC_LT,           KC_GT,           KC_MINS,        KC_UNDS,        KC_COLN, KC_7,         KC_8,         KC_9,         XXXXXXX,
+         XXXXXXX, KC_LT,           KC_GT,           KC_MINS,        KC_UNDS,        KC_COLN, KC_7,         KC_8,         KC_9,         XXXXXXX,
  LGUI_T(KC_PAST), LALT_T(KC_PPLS), LCTL_T(KC_SLSH), LSFT_T(KC_EQL), KC_AMPR,        KC_DOT,  LSFT_T(KC_1), LCTL_T(KC_2), LALT_T(KC_3), LGUI_T(KC_0),
-XXXXXXX, KC_CIRC, KC_TILD,         RALT(KC_E),      KC_DLR,         XXXXXXX,        KC_COMM, KC_4,         KC_5,         KC_6,         XXXXXXX, XXXXXXX,
+XXXXXXX, KC_CIRC, KC_PERC,         RALT(KC_E),      KC_DLR,         XXXXXXX,        KC_COMM, KC_4,         KC_5,         KC_6,         XXXXXXX, XXXXXXX,
                                                   _______, _______, _______,        _______, _______, _______
     ),
 
     [_L3] = LAYOUT(
-KC_GRV,          KC_BSLS,         KC_LBRC,         KC_RBRC,         KC_PIPE,        KC_PGUP, KC_HOME,       SEL_LINE, KC_END,         XXXXXXX,
+         KC_GRV, KC_BSLS,         KC_LBRC,         KC_RBRC,         KC_PIPE,        KC_PGUP, KC_HOME,       XXXXXXX,  KC_END,         XXXXXXX,
 LGUI_T(KC_QUES), LALT_T(KC_EXLM), LCTL_T(KC_LPRN), RSFT_T(KC_RPRN), KC_HASH,        KC_PGDN, KC_LEFT,       KC_UP,    KC_RGHT,        XXXXXXX,
-XXXXXXX, UP_DIR, HOME_DIR,        KC_LCBR,         KC_RCBR,           KC_AT,        XXXXXXX, LCTL(KC_LEFT), KC_DOWN,  LCTL(KC_RIGHT), XXXXXXX, XXXXXXX,
+XXXXXXX, XXXXXXX, KC_TILD,        KC_LCBR,         KC_RCBR,           KC_AT,        XXXXXXX, LCTL(KC_LEFT), KC_DOWN,  LCTL(KC_RIGHT), XXXXXXX, XXXXXXX,
                                                   _______, _______, _______,        _______, _______, _______
     ),
     // clang-format on
@@ -48,17 +42,6 @@ XXXXXXX, UP_DIR, HOME_DIR,        KC_LCBR,         KC_RCBR,           KC_AT,    
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch (keycode) {
-            case UP_DIR:
-                send_string("../");
-                return false;
-            case HOME_DIR:
-                send_string("~/");
-                return false;
-            case SEL_LINE:
-                tap_code(KC_HOME);
-                tap_code16(LSFT(KC_END));
-                return false;
-
             // Workaround for mod tap with non basic keycodes
             case RSFT_T(KC_LPRN):
                 if (record->tap.count && record->event.pressed) {

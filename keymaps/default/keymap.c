23565306dc01c897d8e5d48d0f08f5b6892c73f3
Samuel Born <samuelborn@outlook.de>
2025-11-10 22:33:40 +0100

    fix homerow mods

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -45,13 +45,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             // Workaround for mod tap with non basic keycodes
             case RSFT_T(KC_RPRN):
                 if (record->tap.count && record->event.pressed) {
-                    tap_code16(KC_LPRN);
+                    tap_code16(KC_RPRN);
                     return false;
                 }
                 break;
             case LCTL_T(KC_LPRN):
                 if (record->tap.count && record->event.pressed) {
-                    tap_code16(KC_RPRN);
+                    tap_code16(KC_LPRN);
                     return false;
                 }
                 break;
